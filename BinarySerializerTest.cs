99d0c51e440e073b3d13e3dd4a3ac5e1a9c18370
dhr.b.dekker <dhr.b.dekker@gmail.com>
2020-04-12 10:47:10 +0200

    - Contractless and standard.

--- BinarySerializerTest.cs
+++ BinarySerializerTest.cs
@@ -10,22 +10,23 @@ namespace MsgPack.Repro
 {
     public class BinarySerializerTest
     {
-        private readonly MessagePackSerializerOptions _options = StandardResolver.Options;
+        private readonly MessagePackSerializerOptions _standardOptions = StandardResolver.Options;
+        private readonly MessagePackSerializerOptions _contractlessOptions = ContractlessStandardResolver.Options;
 
         [Fact]
-        public async Task Serialize_And_Deserialize_string_Stream_TestAsync()
+        public async Task Standard_Serialize_And_Deserialize_string_Stream_TestAsync()
         {
             var testObject = "test";
 
             var stream = new MemoryStream();
-            await MessagePackSerializer.SerializeAsync(typeof(string), stream, testObject, _options, default).ConfigureAwait(false);
+            await MessagePackSerializer.SerializeAsync(typeof(string), stream, testObject, _standardOptions, default).ConfigureAwait(false);
 
-            var deserialized = await MessagePackSerializer.DeserializeAsync<string>(stream, _options, default).ConfigureAwait(false);
+            var deserialized = await MessagePackSerializer.DeserializeAsync<string>(stream, _standardOptions, default).ConfigureAwait(false);
             Assert.True(deserialized.Equals(testObject));
         }
 
         [Fact]
-        public async Task Serialize_And_Deserialize_List_Guid_Stream_TestAsync()
+        public async Task Standard_Serialize_And_Deserialize_List_Guid_Stream_TestAsync()
         {
             var testObject = new List<Guid>
             {
@@ -35,9 +36,39 @@ namespace MsgPack.Repro
             };
 
             var stream = new MemoryStream();
-            await MessagePackSerializer.SerializeAsync(typeof(IList<Guid>), stream, testObject, _options, default).ConfigureAwait(false);
+            await MessagePackSerializer.SerializeAsync(typeof(IList<Guid>), stream, testObject, _standardOptions, default).ConfigureAwait(false);
 
-            var deserialized = await MessagePackSerializer.DeserializeAsync<IList<Guid>>(stream, _options, default).ConfigureAwait(false);
+            var deserialized = await MessagePackSerializer.DeserializeAsync<IList<Guid>>(stream, _standardOptions, default).ConfigureAwait(false);
+
+            Assert.True(deserialized.Count is 3);
+        }
+
+        [Fact]
+        public async Task Contractless_Serialize_And_Deserialize_string_Stream_TestAsync()
+        {
+            var testObject = "test";
+
+            var stream = new MemoryStream();
+            await MessagePackSerializer.SerializeAsync(typeof(string), stream, testObject, _contractlessOptions, default).ConfigureAwait(false);
+
+            var deserialized = await MessagePackSerializer.DeserializeAsync<string>(stream, _contractlessOptions, default).ConfigureAwait(false);
+            Assert.True(deserialized.Equals(testObject));
+        }
+
+        [Fact]
+        public async Task Contractless_Serialize_And_Deserialize_List_Guid_Stream_TestAsync()
+        {
+            var testObject = new List<Guid>
+            {
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+            };
+
+            var stream = new MemoryStream();
+            await MessagePackSerializer.SerializeAsync(typeof(IList<Guid>), stream, testObject, _contractlessOptions, default).ConfigureAwait(false);
+
+            var deserialized = await MessagePackSerializer.DeserializeAsync<IList<Guid>>(stream, _contractlessOptions, default).ConfigureAwait(false);
 
             Assert.True(deserialized.Count is 3);
         }
