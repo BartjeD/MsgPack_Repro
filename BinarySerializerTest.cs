8d76910a055302abf24965f20ba8627eed8b0fc1
dhr.b.dekker <dhr.b.dekker@gmail.com>
2020-04-12 16:47:14 +0200

    - Test with wrapper.

--- BinarySerializerTest.cs
+++ BinarySerializerTest.cs
@@ -122,6 +122,28 @@ namespace MsgPack.Repro
         }
 
         [Fact]
+        public async Task Contractless_Serialize_And_Deserialize_Wrapper_And_List_Guid_Stream_TestAsync()
+        {
+            var testObject = new ApiResult<IList<Guid>>
+            {
+                Result = new List<Guid>
+                {
+                    Guid.NewGuid(),
+                    Guid.NewGuid(),
+                    Guid.NewGuid(),
+                },
+            };
+
+            var stream = new MemoryStream();
+            await MessagePackSerializer.SerializeAsync(typeof(ApiResult<IList<Guid>>), stream, testObject, _contractlessOptions, default).ConfigureAwait(false);
+            stream.Position = 0;
+
+            var deserialized = await MessagePackSerializer.DeserializeAsync<ApiResult<IList<Guid>>>(stream, _contractlessOptions, default).ConfigureAwait(false);
+
+            Assert.True(deserialized.Result.Count is 3);
+        }
+
+        [Fact]
         public async Task Contractless_Serialize_And_Deserialize_List_Guid_Bytes_TestAsync()
         {
             var testObject = new List<Guid>
---
+++ Wrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MsgPack.Repro
+{
+    [DataContract]
+    public struct ApiResult<T> : IEquatable<ApiResult<T>>
+    {
+        [DataMember(Order = 0)]
+        public bool? IsSuccesful { get; set; }
+
+        [DataMember(Order = 1)]
+        public IList<string> ErrorCodes { get; set; }
+
+        [DataMember(Order = 2)]
+        public T Result { get; set; }
+
+        public bool HasFailed()
+        {
+            return !IsSuccesful.HasValue || !IsSuccesful.Value;
+        }
+
+        public bool HasSucceeded()
+        {
+            return IsSuccesful is true;
+        }
+
+        public bool Equals(ApiResult<T> other) => other.IsSuccesful == IsSuccesful && other.ErrorCodes == ErrorCodes && ((Result is null && other.Result == null) || Result.Equals(other.Result));
+    }
+}
